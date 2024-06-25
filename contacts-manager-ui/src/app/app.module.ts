2e702bd1ec25a055555a94a745039b08e5ca33cb
RomanShevchukTechStack <roman.shevchuk@tech-stack.io>
2024-06-25 19:36:35 +0300

    added empty components for form, list and details

--- contacts-manager-ui/src/app/app.module.ts
+++ contacts-manager-ui/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactFormComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule,
